57185377c5823069084a40070a4c77bde65eab93
Rikki <rnob643@aucklanduni.ac.nz>
2023-09-14 14:00:21 +1200

    Added Scene Manager
    Initial code for SceneManager.java complete with four initial scenes: mainMenu, timeMachine, lab, and storage.

---
+++ src/main/java/nz/ac/auckland/se206/SceneManager.java
@@ -0,0 +1,39 @@
+package nz.ac.auckland.se206;
+
+import java.util.HashMap;
+import javafx.scene.Parent;
+
+public class SceneManager {
+  public enum AppUi {
+    MAINMENU, TIMEMACHINE, LAB, STORAGE
+  }
+
+  // Initialize hashMap to manage Scenes
+  private static HashMap<AppUi, Parent> sceneMap = new HashMap<AppUi, Parent>();
+
+  // Adds Ui to the hashmap
+  public static void addUi(AppUi ui, Parent root) {
+    sceneMap.put(ui, root);
+  }
+
+  // Returns the root node of a Ui
+  public static Parent getUiRoot(AppUi ui) {
+    return sceneMap.get(ui);
+  }
+
+  // Returns the String value of each scene
+  public static String convertUiType(AppUi ui) {
+    switch (ui) {
+      case MAINMENU:
+        return "mainMenu";
+      case TIMEMACHINE:
+        return "timeMachine";
+      case LAB:
+        return "lab";
+      case STORAGE:
+        return "storage";
+    }
+    return "mainMenu";
+  }
+}
+
