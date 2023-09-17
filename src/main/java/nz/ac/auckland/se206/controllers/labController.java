b5947662c951083c05c51e633bd0e21670f24a89
Rikki <rnob643@aucklanduni.ac.nz>
2023-09-17 14:35:13 +1200

    Added images, updated lab scene
    added the images for storage and lab to the repository. Additionally, started working on lab.fxml and labController.java

--- src/main/java/nz/ac/auckland/se206/controllers/labController.java
+++ src/main/java/nz/ac/auckland/se206/controllers/labController.java
@@ -1,5 +1,18 @@
 package nz.ac.auckland.se206.controllers;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import nz.ac.auckland.se206.App;
+import nz.ac.auckland.se206.SceneManager.AppUi;
+
 public class labController {
-    
+    @FXML private Button btnSwitchToTimeMachine;
+    @FXML private Label lblTimer;
+
+    @FXML
+    private void switchToTimeMachine(ActionEvent event) {
+        App.setUi(AppUi.TIMEMACHINE);
+    }
 }
--- src/main/java/nz/ac/auckland/se206/controllers/mainmenuController.java
+++ src/main/java/nz/ac/auckland/se206/controllers/mainmenuController.java
@@ -1,6 +1,5 @@
 package nz.ac.auckland.se206.controllers;
 
 public class mainmenuController {
-    //CONTROLLER
     
 }
