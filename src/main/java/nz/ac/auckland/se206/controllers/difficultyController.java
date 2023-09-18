f7c2c2f6cfae2e58450b27fc41011fb73bb1a232
Rikki <rnob643@aucklanduni.ac.nz>
2023-09-19 01:17:12 +1200

    Difficulty/timer selection is complete
    Also added a message that will tell the user to select settings befor
    e they are able to start the game.

--- src/main/java/nz/ac/auckland/se206/controllers/difficultyController.java
+++ src/main/java/nz/ac/auckland/se206/controllers/difficultyController.java
@@ -4,6 +4,7 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
+import javafx.scene.control.Label;
 import nz.ac.auckland.se206.App;
 import nz.ac.auckland.se206.SceneManager.AppUi;
 
@@ -11,6 +12,7 @@ public class difficultyController {
   @FXML private Button btnSwitchToTimeMachine;
   @FXML private CheckBox chkbxEasy, chkbxMedium, chkbxHard;
   @FXML private CheckBox chkbxTwoMins, chkbxFourMins, chkbxSixMins;
+  @FXML private Label lblSelectBoxesWarning;
 
   public enum Difficulty {
     EASY, MEDIUM, HARD
@@ -20,96 +22,135 @@ public class difficultyController {
   }
 
   int minutes = 4;
-  boolean isChecked = false;
+  boolean isDifficultyChecked, isTimeChecked = false;
+
   Difficulty currentDifficulty;
   TimeSetting currentTimeSetting;
 
   @FXML
   private void switchToTimeMachine(ActionEvent event) {
-    App.setUi(AppUi.TIMEMACHINE);
+    if (!isDifficultyChecked || !isTimeChecked) {
+      lblSelectBoxesWarning.setText("Select a difficulty and time to begin");
+    } else {
+      App.setUi(AppUi.TIMEMACHINE);
+      // Start timer. Change 'minutes' variable to change the length of the game
+      timemachineController.timemachineStartTimer(minutes);
+      labController.labStartTimer(minutes);
+      storageController.storageStartTimer(minutes);
+    }
 
-    // Start timer. Change 'minutes' variable to change the length of the game
-    timemachineController.timemachineStartTimer(minutes);
-    labController.labStartTimer(minutes);
-    storageController.storageStartTimer(minutes);
+    
   }
 
   @FXML
   private void checkedEasy(ActionEvent event) {
-    if (isChecked == false) {
-      currentDifficulty = Difficulty.EASY;
-      isChecked = true;
-      disableDifficultyBoxes(currentDifficulty, isChecked);
-    } else if (currentDifficulty == Difficulty.EASY) {
-      isChecked = false;
-      disableDifficultyBoxes(Difficulty.EASY, isChecked);
-    } 
+    currentDifficulty = Difficulty.EASY;
+    isDifficultyChecked = true;
+
+    if (chkbxEasy.isSelected()) {
+      deselectDifficultyBoxes(currentDifficulty, isDifficultyChecked);
+    } else if (isDifficultyChecked == true) {
+      isDifficultyChecked = false;
+    }
   }
 
   @FXML
   private void checkedMedium(ActionEvent event) {
-    if (isChecked == false) {
-      currentDifficulty = Difficulty.MEDIUM;
-      isChecked = true;
-      disableDifficultyBoxes(currentDifficulty, isChecked);
-    } else if (currentDifficulty == Difficulty.MEDIUM) {
-      isChecked = false;
-      disableDifficultyBoxes(Difficulty.MEDIUM, isChecked);
-    } 
+    currentDifficulty = Difficulty.MEDIUM;
+    isDifficultyChecked = true;
+
+    if (chkbxMedium.isSelected()) {
+      deselectDifficultyBoxes(currentDifficulty, isDifficultyChecked);
+    } else if (isDifficultyChecked == true) {
+      isDifficultyChecked = false;
+    }
   }
 
   @FXML
   private void checkedHard(ActionEvent event) {
-    if (isChecked == false) {
-      currentDifficulty = Difficulty.HARD;
-      isChecked = true;
-      disableDifficultyBoxes(currentDifficulty, isChecked);
-    } else if (currentDifficulty == Difficulty.HARD) {
-      isChecked = false;
-      disableDifficultyBoxes(Difficulty.HARD, isChecked);
-    } 
+    currentDifficulty = Difficulty.HARD;
+    isDifficultyChecked = true;
+
+    if (chkbxHard.isSelected()) {
+      deselectDifficultyBoxes(currentDifficulty, isDifficultyChecked);
+    } else if (isDifficultyChecked == true) {
+      isDifficultyChecked = false;
+    }
   }
 
   @FXML
   private void checkedTwoMins() {
-    if (isChecked = false) {
-      currentTimeSetting = TimeSetting.TWO;
-      isChecked = true;
-    } 
+    currentTimeSetting = TimeSetting.TWO;
+    isTimeChecked = true;
+
+    if (chkbxTwoMins.isSelected()) {
+      deselectTimeBoxes(currentTimeSetting, isTimeChecked);
+    } else if (isTimeChecked == true) {
+      isTimeChecked = false;
+    }
+
+    minutes = 2;
   }
 
   @FXML
   private void checkedFourMins() {
-    if (isChecked = false) {
-      currentTimeSetting = TimeSetting.FOUR;
-      isChecked = true;
+    currentTimeSetting = TimeSetting.FOUR;
+    isTimeChecked = true;
+
+    if (chkbxFourMins.isSelected()) {
+      deselectTimeBoxes(currentTimeSetting, isTimeChecked);
+    } else if (isTimeChecked == true) {
+      isTimeChecked = false;
     }
+
+    minutes = 4;
   }
   
   @FXML
   private void checkedSixMins() {
-    if (isChecked = false) {
-      currentTimeSetting = TimeSetting.SIX;
-      isChecked = true;
+    currentTimeSetting = TimeSetting.SIX;
+    isTimeChecked = true;
+
+    if (chkbxSixMins.isSelected()) {
+      deselectTimeBoxes(currentTimeSetting, isTimeChecked);
+    } else if (isTimeChecked == true) {
+      isTimeChecked = false;
     }
+
+    minutes = 6;
   }
 
-  private void disableDifficultyBoxes(Difficulty difficulty, Boolean isChecked) {
+  private void deselectDifficultyBoxes(Difficulty difficulty, Boolean isDifficultyChecked) {
     switch (difficulty) {
       case EASY:
-       System.out.println("EASY");
-        chkbxMedium.setDisable(isChecked);
-        chkbxHard.setDisable(isChecked);
+        chkbxMedium.setSelected(false);
+        chkbxHard.setSelected(false);
         break;
       case MEDIUM:
-        chkbxEasy.setDisable(isChecked);
-        chkbxHard.setDisable(isChecked);
+        chkbxEasy.setSelected(false);
+        chkbxHard.setSelected(false);
         break;
       case HARD:
-        chkbxEasy.setDisable(isChecked);
-        chkbxMedium.setDisable(isChecked);
+        chkbxEasy.setSelected(false);
+        chkbxMedium.setSelected(false);
         break;
     }
   }
-
+  
+  private void deselectTimeBoxes(TimeSetting timeSetting, Boolean isTimeChecked) {
+    switch (timeSetting) {
+      case TWO:
+        chkbxFourMins.setSelected(false);
+        chkbxSixMins.setSelected(false);
+        break;
+      case FOUR:
+        chkbxTwoMins.setSelected(false);
+        chkbxSixMins.setSelected(false);
+        break;
+      case SIX:
+        chkbxTwoMins.setSelected(false);
+        chkbxFourMins.setSelected(false);
+        break;
+    }
+  }
 }
