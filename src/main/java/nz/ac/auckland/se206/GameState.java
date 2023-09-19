26e6692b61f0bbfa61fee2a930e9acc667cab701
Quaid Sage <qsag699@aucklanduni.ac.nz>
2023-09-19 16:08:03 +1200

    Implement AI interface for time machine scene
    Implements the AI interface which contains a chat box for the user to
    enter messages and communicate with the OpenAI ChatGPT API. This
    interface is currently only on the time machine scene.

--- src/main/java/nz/ac/auckland/se206/GameState.java
+++ src/main/java/nz/ac/auckland/se206/GameState.java
@@ -1,5 +1,7 @@
 package nz.ac.auckland.se206;
 
+import nz.ac.auckland.se206.speech.TextToSpeech;
+
 /** Represents the state of the game. */
 public class GameState {
 
@@ -8,4 +10,15 @@ public class GameState {
 
   /** Indicates whether the key has been found. */
   public static boolean isKeyFound = false;
+
+  /** Current text to speech for game */
+  public static TextToSpeech txtToSpeech;
+
+  /** Current item to be found in the game */
+  public static String item = "battery";
+
+  /** Returns the current required item for the game. */
+  public static String getItem() {
+    return item;
+  }
 }
--- src/main/java/nz/ac/auckland/se206/controllers/timemachineController.java
+++ src/main/java/nz/ac/auckland/se206/controllers/timemachineController.java
@@ -1,39 +1,227 @@
 package nz.ac.auckland.se206.controllers;
 
+import java.io.IOException;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.application.Platform;
+import javafx.concurrent.Task;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.util.Duration;
 import nz.ac.auckland.se206.App;
+import nz.ac.auckland.se206.GameState;
 import nz.ac.auckland.se206.SceneManager.AppUi;
+import nz.ac.auckland.se206.gpt.ChatMessage;
+import nz.ac.auckland.se206.gpt.GptPromptEngineering;
+import nz.ac.auckland.se206.gpt.openai.ApiProxyException;
+import nz.ac.auckland.se206.gpt.openai.ChatCompletionRequest;
+import nz.ac.auckland.se206.gpt.openai.ChatCompletionResult;
+import nz.ac.auckland.se206.gpt.openai.ChatCompletionResult.Choice;
 
 public class timemachineController {
-    @FXML private Button btnSwitchToLab, btnSwitchToStorage;
-    @FXML private Label lblTimer;
+  @FXML private Button btnSwitchToLab, btnSwitchToStorage, btnSend;
+  @FXML private Label lblTimer;
+  @FXML private TextArea chatArea;
+  @FXML private TextArea chatField;
 
-    private static timerController timer = new timerController();
+  private ChatCompletionRequest chatCompletionRequest;
+  private int characterDelay = 5;
 
-    public void initialize() {
-        // Bind the lblTimer to the timerController properties.
-        lblTimer.textProperty().bind(timer.messageProperty());
-        timer.setOnSucceeded(e -> {
-            // Add code here to implement the loss of the game
-            lblTimer.setText("0:00");
+  private static timerController timer = new timerController();
+
+  public void initialize() {
+    // Initialise AI
+    chatCompletionRequest =
+        new ChatCompletionRequest().setN(1).setTemperature(0.2).setTopP(0.5).setMaxTokens(100);
+
+    // Create riddle chat thread
+    Task<ChatMessage> riddleTask =
+        createTask(GptPromptEngineering.getRiddleWithGivenWord(GameState.getItem()));
+    Thread riddleThread = new Thread(riddleTask);
+    riddleThread.start();
+    riddleTask.setOnSucceeded(
+        e -> {
+          appendChatMessage(riddleTask.getValue());
         });
+
+    // Bind the lblTimer to the timerController properties.
+    lblTimer.textProperty().bind(timer.messageProperty());
+    timer.setOnSucceeded(
+        e -> {
+          // Add code here to implement the loss of the game
+          lblTimer.setText("0:00");
+        });
+  }
+
+  @FXML
+  private void switchToLab(ActionEvent event) {
+    App.setUi(AppUi.LAB);
+  }
+
+  @FXML
+  private void switchToStorage(ActionEvent event) {
+    App.setUi(AppUi.STORAGE);
+  }
+
+  public static void timemachineStartTimer(int minutes) {
+    timer.setMinutes(minutes);
+    timer.start();
+  }
+
+  /**
+   * Creates a task to run theLLM model on a given message to be run by background thread.
+   *
+   * @param message string to attach to message to be given to the LLM
+   */
+  private Task<ChatMessage> createTask(String message) {
+    Task<ChatMessage> task =
+        new Task<ChatMessage>() {
+          @Override
+          protected ChatMessage call() throws Exception {
+            btnSend.setDisable(true);
+            ChatMessage msg = runGpt(new ChatMessage("assistant", message));
+            Platform.runLater(
+                () -> {
+                  System.out.println("Initialised message");
+                });
+            return msg;
+          }
+        };
+    return task;
+  }
+
+  /**
+   * Appends a chat message to the chat text area one character at a time.
+   *
+   * @param msg the chat message to append
+   */
+  public void appendChatMessage(ChatMessage msg) {
+    chatArea.appendText("\n\n- ");
+
+    // Disable send button
+    btnSend.setDisable(true);
+
+    // Convert message to char array
+    char[] ch = msg.getContent().toCharArray();
+
+    // Use text to speech alongside chat appending
+    Task<Void> txtSpeechTask =
+        new Task<Void>() {
+          @Override
+          protected Void call() throws Exception {
+            GameState.txtToSpeech.speak(msg.getContent());
+            return null;
+          }
+        };
+
+    Thread txtSpeechThread = new Thread(txtSpeechTask);
+    txtSpeechThread.start();
+
+    // Create a timeline and keyframes to append each character of the message to the chat text area
+    Timeline timeline = new Timeline();
+    if (ch.length < 100) {
+      characterDelay = (50 - (ch.length / 2)) + 5;
     }
-    
-    @FXML
-    private void switchToLab(ActionEvent event) {
-        App.setUi(AppUi.LAB);
+    System.out.println("printing with delay of: " + characterDelay);
+    Duration delayBetweenCharacters = Duration.millis(characterDelay);
+    Duration frame = delayBetweenCharacters;
+    for (int i = 0; i < ch.length; i++) {
+      final int I = i;
+      KeyFrame keyFrame =
+          new KeyFrame(
+              frame,
+              event -> {
+                chatArea.appendText(String.valueOf(ch[I]));
+              });
+      timeline.getKeyFrames().add(keyFrame);
+      frame = frame.add(delayBetweenCharacters);
     }
 
-    @FXML
-    private void switchToStorage(ActionEvent event) {
-        App.setUi(AppUi.STORAGE);
-    }
+    // Play timeline animation
+    timeline.play();
 
-    public static void timemachineStartTimer(int minutes) {
-        timer.setMinutes(minutes);
-        timer.start();
+    // Enable send button after animation is finished
+    timeline.setOnFinished(
+        event -> {
+          btnSend.setDisable(false);
+        });
+  }
+
+  /**
+   * Delays given code by a given number of milliseconds.
+   *
+   * @param ms milliseconds of delay
+   * @param continuation Code to execute after delay
+   */
+  public static void delay(int ms, Runnable continuation) {
+    // Create delay function
+    Task<Void> delayTask =
+        new Task<Void>() {
+          @Override
+          protected Void call() throws Exception {
+            try {
+              Thread.sleep(ms);
+            } catch (InterruptedException e) {
+            }
+            return null;
+          }
+        };
+    // Execute code after delay
+    delayTask.setOnSucceeded(event -> continuation.run());
+
+    // Start delay thread
+    new Thread(delayTask).start();
+  }
+
+  /**
+   * Runs the GPT model with a given chat message.
+   *
+   * @param msg the chat message to process
+   * @return the response chat message
+   * @throws ApiProxyException if there is an error communicating with the API proxy
+   */
+  private ChatMessage runGpt(ChatMessage msg) throws ApiProxyException {
+    chatCompletionRequest.addMessage(msg);
+    try {
+      ChatCompletionResult chatCompletionResult = chatCompletionRequest.execute();
+      Choice result = chatCompletionResult.getChoices().iterator().next();
+      chatCompletionRequest.addMessage(result.getChatMessage());
+      return result.getChatMessage();
+    } catch (ApiProxyException e) {
+      e.printStackTrace();
+      return null;
     }
+  }
+
+  /**
+   * Sends a message to the GPT model.
+   *
+   * @param event the action event triggered by the send button
+   * @throws ApiProxyException if there is an error communicating with the API proxy
+   * @throws IOException if there is an I/O error
+   */
+  @FXML
+  private void onSendMessage(ActionEvent event) throws ApiProxyException, IOException {
+    String message = chatField.getText();
+    if (message.trim().isEmpty()) {
+      System.out.println("message is empty");
+      return;
+    }
+    chatField.clear();
+
+    ChatMessage chatMessage = new ChatMessage("user", message);
+    appendChatMessage(chatMessage);
+
+    Task<ChatMessage> chatTask = createTask(message);
+    Thread chatThread = new Thread(chatTask);
+    chatThread.start();
+
+    chatTask.setOnSucceeded(
+        e -> {
+          appendChatMessage(chatTask.getValue());
+        });
+  }
 }
