71b62a1dd032e21c8b901cf463146d81f265d062
SHAURYAP <spt853@aucklanduni.ac.nz>
2023-09-21 18:39:59 +1200

    incorporated minigame into storage controller successfully.

--- src/main/java/nz/ac/auckland/se206/controllers/storageController.java
+++ src/main/java/nz/ac/auckland/se206/controllers/storageController.java
@@ -1,18 +1,25 @@
 package nz.ac.auckland.se206.controllers;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Random;
 import javafx.animation.KeyFrame;
+import javafx.animation.PauseTransition;
 import javafx.animation.Timeline;
 import javafx.application.Platform;
 import javafx.concurrent.Task;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.Control;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.VBox;
 import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Text;
 import javafx.util.Duration;
 import nz.ac.auckland.se206.App;
 import nz.ac.auckland.se206.GameState;
@@ -32,6 +39,37 @@ public class storageController {
   @FXML private TextArea chatArea;
   @FXML private TextArea chatField;
   @FXML private ImageView imgScientistThinking;
+  @FXML private Rectangle circuitGameBg;
+  @FXML private ImageView circuitGameImg;
+  @FXML private VBox memoryGame;
+  @FXML private Button btnStartCircuitGame;
+  @FXML private Text text;
+  @FXML private Button button0;
+  @FXML private Button button1;
+  @FXML private Button button2;
+  @FXML private Button button3;
+  @FXML private Button button4;
+  @FXML private Button button5;
+  @FXML private Button button6;
+  @FXML private Button button7;
+  @FXML private Button button8;
+
+  private ArrayList<String> possibleButtons =
+      new ArrayList<>(
+          Arrays.asList(
+              "button0", "button1", "button2", "button3", "button4", "button5", "button6",
+              "button7", "button8"));
+
+  private ArrayList<Button> buttons = new ArrayList<>();
+
+  private ArrayList<String> pattern = new ArrayList<>();
+
+  private int patternOrder = 0;
+
+  private Random random = new Random();
+
+  private int counter = 0;
+  private int turn = 1;
 
   // Initialise Variables
   private int characterDelay = 5;
@@ -50,6 +88,97 @@ public class storageController {
         });
 
     createUpdateTask();
+
+    buttons.addAll(
+        Arrays.asList(
+            button0, button1, button2, button3, button4, button5, button6, button7, button8));
+  }
+
+  @FXML
+  void buttonClicked(ActionEvent event) {
+
+    if (((Control) event.getSource()).getId().equals(pattern.get(counter))) {
+      text.setText("Current Streak: " + counter);
+      Button button = buttons.get(getIndexOfButton(event));
+      changeButtonColor(button, "-fx-base: lightgreen");
+      counter++;
+    } else {
+      Button button = buttons.get(getIndexOfButton(event));
+      changeButtonColor(button, "-fx-base: red");
+      text.setText("Wrong");
+      return;
+    }
+
+    if (counter == turn) {
+      nextTurn();
+    }
+  }
+
+  @FXML
+  void start(ActionEvent event) {
+    pattern.clear();
+
+    pattern.add(possibleButtons.get(random.nextInt(possibleButtons.size())));
+    showPattern();
+    System.out.println(pattern);
+
+    counter = 0;
+    turn = 1;
+  }
+
+  private void nextTurn() {
+    counter = 0;
+    turn++;
+
+    pattern.add(possibleButtons.get(random.nextInt(possibleButtons.size())));
+    showPattern();
+    System.out.println(pattern);
+  }
+
+  private int getIndexOfButton(ActionEvent event) {
+    String buttonId = ((Control) event.getSource()).getId();
+    return Integer.parseInt(buttonId.substring(buttonId.length() - 1));
+  }
+
+  private int getIndexOfButton(String button) {
+    return Integer.parseInt(button.substring(button.length() - 1));
+  }
+
+  private void showPattern() {
+    PauseTransition pause = new PauseTransition(Duration.seconds(1));
+    pause.setOnFinished(
+        e -> {
+          Timeline timeline =
+              new Timeline(
+                  new KeyFrame(
+                      Duration.seconds(1),
+                      event -> {
+                        showNext();
+                      }));
+          timeline.setCycleCount(pattern.size());
+          timeline.play();
+        });
+    pause.play();
+  }
+
+  private void showNext() {
+    Button button = buttons.get(getIndexOfButton(pattern.get(patternOrder)));
+    changeButtonColor(button, "-fx-base: gray");
+    patternOrder++;
+
+    if (patternOrder == turn) {
+      patternOrder = 0;
+    }
+  }
+
+  private void changeButtonColor(Button button, String color) {
+    button.setStyle(color);
+    PauseTransition pause = new PauseTransition(Duration.seconds(0.5));
+    pause.setOnFinished(
+        e -> {
+          button.setStyle(null);
+        });
+    pause.play();
   }
 
   /**
@@ -69,6 +198,12 @@ public class storageController {
     background.setVisible(false);
     circuitBox.setVisible(false);
     circuitBoxImg.setVisible(false);
+
+    circuitGameBg.setVisible(true);
+    circuitGameImg.setVisible(true);
+    memoryGame.setVisible(true);
+    btnStartCircuitGame.setVisible(true);
+    text.setVisible(true);
   }
 
   /**
